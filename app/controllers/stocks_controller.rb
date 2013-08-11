fece94273b183b6c4fb62d0bebee2d9da81b2d27
Matthew Tschoegl <matthewtschoegl@Matthews-MacBook-Pro.local>
2013-08-11 18:58:12 -0500

    Connected /results to /picker.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,12 +5,13 @@ class StocksController < ApplicationController
   end
 
   def picker
-     @stock_proper_name = params[:stock_symbol]
-     @stock_symbol_name = params[:stock_tbd]
+  	@stock_proper_name = params[:stock_symbol]
+    @stock_symbol_name = params[:stock_tbd]
   end
 
   def results
-
+  	@mod_stock_name = params[:mod_stock_fullName]
+  	@mod_stock_symbol = params[:mod_stock_fullSymbol]
   end
 
 end
