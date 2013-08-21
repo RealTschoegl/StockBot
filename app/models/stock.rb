6035a949bb54241c20f86201d8078ebd3683b0a5
Matthew Tschoegl <matthewtschoegl@Matthews-MacBook-Pro.local>
2013-08-21 00:58:37 -0500

    Further decomposed models.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,126 +3,115 @@ class Stock < ActiveRecord::Base
 
   	require 'yahoofinance' # Needed for current price
 
-    def self.get_stock_price(stock_symbol_name)
+    # These are the variables that the valuation uses
+    @@risk_free_rate = 0.0141 #5 year treasury yield at market
+    @@market_growth_rate = 0.2038 #S&P500 1 year growth rate
+    @@years_horizon = 10
+
+      @stock_ticker_symbol = ""
+      @current_stock_price = 0
+      @free_cash_flow = 0
+      @num_shares = 0
+      @PE_ratio = 0
+      @dividend_per_share = 0
+      @dividend_growth_rate = 0
+      @beta = 0
+      @cost_of_equity = 0
+      @rate_of_return = 0
+      @fcf_share_value = 0
+      @capm_share_value = 0
+      @dividend_share_value = 0
+      @composite_share_value = 0
+
+
+    # This method is used to computer the StockBot "house" valuation
+    def self.compute_stock_price(stock_ticker_symbol)
+      Stock.get_complex_constants(stock_ticker_symbol)
+      Stock.get_composite_share_value
+      Stock.get_time_value_of_money
+
+      return @composite_share_value
+    end
+
+    def self.get_complex_constants(stock_symbol_name)
+      Stock.get_cost_of_equity
+      Stock.get_current_stock_price
+      Stock.get_rate_of_return
+    end
+
+    # This method is used to get the name of the stock that the user submits and match it to the name of a stock in our database
+    def self.get_stock_ticker_symbol(stock_symbol_name)
+      @stock_ticker_symbol = Stock.where(:stock_ticker => stock_symbol_name).first.stock_ticker
+    end
+
+    # This method is used to get the current stock price in the market
+    def self.get_current_stock_price(stock_symbol_name)
       quote_type = YahooFinance::StandardQuote
-      quote_symbols = stock_symbol_name
-      YahooFinance::get_quotes( quote_type, quote_symbols ) do |qt|
-        return "#{qt.lastTrade+1}"
+      quote_symbol = stock_symbol_name
+      YahooFinance::get_quotes( quote_type, quote_symbol ) do |qt|
+        @current_stock_price = qt.lastTrade
       end
     end 
 
-    def self.naming_test(stock_symbol_name)
-    	Stock.where(:stock_ticker => stock_symbol_name).first.stock_ticker
+    def self.get_free_cash_flow(stock_symbol_name)
+      @free_cash_flow = Stock.where(:stock_ticker => stock_symbol_name).first.free_cash_flow
     end
 
-  	# @company_data = {
-  	# 	 :stock_ticker_m => # @stock_ticker_symbol,
-  	# 	 :company_m => # SELECT :company FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :free_cash_flow_m => # SELECT :free_cash_flow FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :num_shares_m => # SELECT :num_shares FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :PE_ratio_m => # SELECT :PE_ratio_m FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :dividend_per_share_m => # SELECT :dividend_per_share FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :dividend_growth_rate_m => # SELECT :dividened_growth_rate FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :beta_m => # SELECT :beta FROM Stocks WHERE :stock_ticker_m == :stock_ticker, 
-  	# 	 :cost_of_equity_m => 0, 
-  	# 	 :current_stock_price_m => 0, 
-  	# 	 :expected_share_value_m => 0, 
-  	# 	 :capm_share_value_m => 0, 
-  	# 	 :dividend_share_value_m => 0, 
-  	# 	 :composite_share_value_m => 0
-  	# }
+    def self.get_num_shares(stock_symbol_name)
+      @num_shares = Stock.where(:stock_ticker => stock_symbol_name).first.num_shares
+    end
 
-	# @@risk_free_rate = 0.0141 #5 year treasury yield at market
-	# @@market_growth_rate = 0.2038 #S&P500 1 year growth rate
+    def self.get_PE_ratio(stock_symbol_name)
+      @PE_ratio = Stock.where(:stock_ticker => stock_symbol_name).first.free_cash_flow / 100
+    end
 
-	# # This is the catchall that provides a complete valuation
-	# def composite_valuation
-	# 	free_cash_flow_method
-	# 	dividend_discount_model
-	# 	capm_method
-	# 	@company_data.map do |category, value|
-	# 		if value[:dividend_per_share_m] == 0
-	# 			value[:composite_share_value_m] = ((value[:expected_share_value_m].to_f + value[:capm_share_value_m].to_f)/2).round(2)
-	# 		else
-	# 			value[:composite_share_value_m] = ((value[:expected_share_value_m].to_f + value[:dividend_share_value_m].to_f + value[:capm_share_value_m].to_f)/3).round(2)
-	# 		end
-	# 	end
-	# 	time_value_of_money
-	# end
+    def self.get_dividend_per_share(stock_symbol_name)
+      @dividend_per_share = Stock.where(:stock_ticker => stock_symbol_name).first.dividend_per_share
+    end
 
-	# # Vales the stock based on the FCF approach
-	# def free_cash_flow_method
-	# 	get_cost_of_equity
-	# 	@company_data.map do |category, value|
-	# 		value[:expected_share_value_m] += value[:free_cash_flow_m]/(value[:cost_of_equity_m]-(value[:PE_ratio_m]/100))
-	# 	end
-	# 	get_num_shares
-	# end
+    def self.get_dividend_growth_rate(stock_symbol_name)
+      @dividend_growth_rate = Stock.where(:stock_ticker => stock_symbol_name).first.dividend_growth_rate
+    end
 
-	# # Value the stock based on the dividend discount method
-	
-	# def dividend_discount_model
-	# 	get_stock_prices
-	# 	@company_data.map do |category, value|
-	# 		rate_of_return = (value[:dividend_per_share_m]/value[:current_stock_price_m]) + value[:dividend_growth_rate_m]
-	# 		value[:dividend_share_value_m] += value[:dividend_per_share_m]/(rate_of_return - value[:dividend_growth_rate_m])
-	# 	end
-	# end
-	
-	# # Gets the present real price of the stock
-	# def get_stock_prices
-	# 	@company_data.each do |category, value|
-	# 		quote_type = YahooFinance::StandardQuote
-	# 		quote_symbols = "#{category}"
-	# 		YahooFinance::get_quotes(quote_type, quote_symbols) do |qt|
-	# 			value[:current_stock_price_m] = qt.lastTrade
-	# 		end
-	# 	end
-	# end
-	
-	# # Finds the cost of equity for stock
-	# def get_cost_of_equity
-	# 	@company_data.map do |category, value|
-	# 		value[:cost_of_equity_m] = @@risk_free_rate + value[:beta_m] * (@@market_growth_rate - @@risk_free_rate)
-	# 	end
-	# end
+    def self.get_beta(stock_symbol_name)
+      @beta = Stock.where(:stock_ticker => stock_symbol_name).first.beta
+    end
 
-	# # Values the stock based on the CAPM method
-	# def capm_method
-	# 	capm_years = @years
-	# 	get_cost_of_equity
-	# 	get_stock_prices
-	# 	@company_data.map do |category, value|
-	# 		value[:capm_share_value_m] = (value[:cost_of_equity_m] + 1) * capm_years * value[:current_stock_price_m]
-	# 	end
-	# end
-	# # Divides the value of the stock by the number of shares - useful for FCF 
-	# def get_num_shares
-	# 	puts @company_data.class
-	# 	@company_data.map do |category, value|
-	# 		value[:expected_share_value_m] = (value[:expected_share_value_m].to_f/value[:num_shares_m].to_f).round(2)
-	# 	end
-	# end
+    def self.get_cost_of_equity
+      @cost_of_equity =  @@risk_free_rate + @beta * (@@market_growth_rate - @@risk_free_rate)
+    end
 
-	# # Asks the user for a time horizon 
-	# def years
-	# 	puts "How many years do you want to hold the stock for?"
-	# 	@years = gets.chomp.to_f
-	# end
+    def self.get_fcf_share_value
+      @fcf_share_value = @free_cash_flow / (@cost_of_equity - @PE_ratio)
+    end
 
-	# # Puts the company and it's values
-	# def test
-	# 	@company_data.each do |category, value|
-	# 		puts category
-	# 		puts value
-	# 	end
-	# 	return nil
-	# end
+    def self.get_capm_share_value
+      @capm_share_value = (@cost_of_equity + 1) * @@years_horizon * @current_stock_price
+    end
 
-	# # Takes the future value of the stock and changes it into the present value
-	# def time_value_of_money
-	# 	value[:composite_share_value_m] = value[:composite_share_value_m]/((1+@@market_growth_rate)**@years)
-	# end
+    def self.get_rate_of_return
+      @rate_of_return = (@dividend_per_share / @current_stock_price) + @dividend_growth_rate
+    end
+
+    def self.get_dividend_share_value
+      @dividend_share_value = @dividend_per_share/(@rate_of_return - @dividend_growth_rate)
+    end
+
+    def self.get_composite_share_value
+        if @dividend_per_share == 0
+          @composite_share_value = (@fcf_share_value + @capm_share_value) / 2
+        else
+          @composite_share_value = (@fcf_share_value + @dividend_share_value + @capm_share_value) / 3
+        end
+    end
+
+    def self.get_time_value_of_money
+      @composite_share_value = @composite_share_value / ((1 + @@market_growth_rate)**@@years_horizon)
+    end
+
+    def self.divide_by_num_shares
+      @fcf_share_value = @fcf_share_value / @num_shares
+    end
 	
 end
 
