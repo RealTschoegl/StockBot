8d29444ff74c7cd5e83982fed9bc0b8a5e4f1a98
Matthew Tschoegl <matthewtschoegl@Matthews-MacBook-Pro.local>
2013-08-29 00:27:04 -0500

    Added a docs portion.  Created the picker inputs and linked them to the constants in the ModValue class.  Added a JS file to create a counter for a picker input.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -19,6 +19,18 @@ class StocksController < ApplicationController
   	@mod_stock_name = params[:mod_stock_fullName]
   	@mod_stock_symbol = params[:mod_stock_fullSymbol]
     mod_stock = ValuationEngine::ModValue.new(params[:mod_stock_fullSymbol])
+
+    # Public: An integer value that represents the number of years from now that we would intend to sell the stock.  Here we use the number of years that the user would like to hold onto the stock.
+    mod_stock.class.years_horizon = (params[:mod_stock_yearHorizon]).to_i
+
+    # Public: A float value to four digits that represents the lowest possible borrowing value. Here we use the 5 year treasury yield at market as the base value and adjust it based on how they think the US gov't will manage the money supply.
+    mod_stock.class.risk_free_rate = (0.0141 * (params[:mod_stock_riskFreeRate]).to_f).round(4)
+
+    # Public: A float value to four digits that represents the amount that the market grew in the previous year. Here we use the S&P500 1 year growth rate as the base rate and adjust it with the user's estimate of how the market will do over the period that they intend to hold the stock for.
+    mod_stock.class.market_growth_rate = (0.2038 + (params[:mod_stock_marketGrowthRate]).to_f/100).round(4)
+
+    @stuffington = mod_stock
+
   	@mod_stock_value = mod_stock.compute_stock_price
   end
 
--- lib/modules/valuation_engine.rb
+++ lib/modules/valuation_engine.rb
@@ -3,16 +3,52 @@ require 'yahoofinance'
 module ValuationEngine
 
   class Value
-    attr_accessor :company, :stock_ticker, :current_stock_price, :free_cash_flow, :num_shares, :PE_ratio, :dividend_per_share, :dividend_growth_rate, :beta, :cost_of_equity, :rate_of_return, :fcf_share_value, :capm_share_value, :dividend_share_value, :composite_share_value
+    # Public: Gets/Sets the String name of the stock's DBA name.
+    attr_accessor :company
+    # Public: Gets/Sets the String name of the stock's ticker symbol.
+    attr_accessor :stock_ticker
+    # Public: Gets/Sets the Float value of the stock's last traded price per share.
+    attr_accessor :current_stock_price
+    # Public: Gets/Sets the Integer value of the stock's free cash flow.
+    attr_accessor :free_cash_flow
+    # Public: Gets/Sets the Integer value of the stock's number of public shares.
+    attr_accessor :num_shares
+    # Public: Gets/Sets the Float value of the stock's price-to-equity ratio.
+    attr_accessor :PE_ratio
+    # Public: Gets/Sets the Float value of the stock's dividend payout per share.
+    attr_accessor :dividend_per_share
+    # Public: Gets/Sets the Float value of the stock's dividend growth rate this year.
+    attr_accessor :dividend_growth_rate
+    # Public: Gets/Sets the Float value of the stock's performance versus the broader market.
+    attr_accessor :beta
+    # Public: Gets/Sets the Float value of the stock's cost of equity.
+    attr_accessor :cost_of_equity
+    # Public: Gets/Sets the Float value of the stock's rate of return.
+    attr_accessor :rate_of_return
+    # Public: Gets/Sets the Float value of the stock's free cash flow value per share.
+    attr_accessor :fcf_share_value
+    # Public: Gets/Sets the Float value of the stock's CAPM value per share.
+    attr_accessor :capm_share_value
+    # Public: Gets/Sets the Float value of the stock's rate of return.
+    attr_accessor :dividend_share_value
+    # Public: Gets/Sets the Float value of the stock's value as a composite of the fcf_share_value, capm_share_value, and dividend_share_value.
+    attr_accessor :composite_share_value
 
     class << self
-      attr_accessor :risk_free_rate, :market_growth_rate, :years_horizon
+      # Public: Gets/Sets the @risk_free_rate.
+      attr_accessor :risk_free_rate
+      # Public: Gets/Sets the @market_growth_rate.
+      attr_accessor :market_growth_rate
+      # Public: Gets/Sets the @years_horizon.
+      attr_accessor :years_horizon
     end
 
-  	# These are the variables that the valuation uses
-    @risk_free_rate = 0.0141 #5 year treasury yield at market
-    @market_growth_rate = 0.2038 #S&P500 1 year growth rate
-    @years_horizon = 10 # Hard coded for now as a year value
+  	# Public: A float value to four digits that represents the lowest possible borrowing value.  Here we use the 5 year treasury yield at market as that value.
+    @risk_free_rate = 0.0141 
+    # Public: A float value to four digits that represents the amount that the market grew in the previous year.  Here we use the S&P500 1 year growth rate.
+    @market_growth_rate = 0.2038 
+    # Public: An integer value that represents the number of years from now that we would intend to sell the stock.
+    @years_horizon = 10
 
   	def initialize(submitted_stock)
       @stock_ticker = submitted_stock
@@ -60,48 +96,46 @@ module ValuationEngine
     end
 
     def get_cost_of_equity
-      self.cost_of_equity =  self.class.risk_free_rate + self.beta * (self.class.market_growth_rate - self.class.risk_free_rate)
+      self.cost_of_equity =  (self.class.risk_free_rate + self.beta * (self.class.market_growth_rate - self.class.risk_free_rate)).round(2)
     end
 
     def get_fcf_share_value
-      self.fcf_share_value = self.free_cash_flow / (self.cost_of_equity - self.PE_ratio)
+      self.fcf_share_value = (self.free_cash_flow / (self.cost_of_equity - self.PE_ratio)).round(2)
       divide_by_num_shares
     end
 
     def get_capm_share_value
-      self.capm_share_value = (self.cost_of_equity + 1) * self.class.years_horizon * self.current_stock_price
+      self.capm_share_value = ((self.cost_of_equity + 1) * self.class.years_horizon * self.current_stock_price).round(2)
     end
 
     def get_rate_of_return
-      self.rate_of_return = (self.dividend_per_share / self.current_stock_price) + self.dividend_growth_rate
+      self.rate_of_return = ((self.dividend_per_share / self.current_stock_price) + self.dividend_growth_rate).round(2)
     end
 
     def get_dividend_share_value
-      self.dividend_share_value = self.dividend_per_share/(self.rate_of_return - self.dividend_growth_rate)
+      self.dividend_share_value = (self.dividend_per_share/(self.rate_of_return - self.dividend_growth_rate)).round(2)
     end
 
     def get_composite_share_value
         if self.dividend_per_share == 0
-          self.composite_share_value = (self.get_fcf_share_value + self.get_capm_share_value) / 2
+          self.composite_share_value = ((self.get_fcf_share_value + self.get_capm_share_value) / 2).round(2)
         else
-          self.composite_share_value = (self.get_fcf_share_value + self.get_dividend_share_value + self.get_capm_share_value) / 3
+          self.composite_share_value = ((self.get_fcf_share_value + self.get_dividend_share_value + self.get_capm_share_value) / 3).round(2)
         end
     end
 
     def get_time_value_of_money
-      self.composite_share_value = self.composite_share_value / ((1 + self.class.market_growth_rate)**self.class.years_horizon)
+      self.composite_share_value = (self.composite_share_value / ((1 + self.class.market_growth_rate)**self.class.years_horizon)).round(2)
     end
 
     def divide_by_num_shares
-      self.fcf_share_value = self.fcf_share_value / self.num_shares
+      self.fcf_share_value = (self.fcf_share_value / self.num_shares).round(2)
     end 
 
   end
 
   class ModValue < Value
-    @risk_free_rate = 0.0141 + 0.01
-    @market_growth_rate = 0.2038 + 0.02
-    @years_horizon = 10 + 5
+    
   end
 
 end
