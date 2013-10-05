b70de9e7e9e77b7ae24eddc2979ecd6b0045e86d
RealTschoegl <Matt.Tschoegl@gmail.com>
2013-10-04 19:21:56 -0500

    Produced index page for companies.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,10 +4,6 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @companies }
-    end
   end
 
   # GET /companies/1
