ddd5103bbea03c47fda8b6df0fd34b703e9012c6
RealTschoegl <Matt.Tschoegl@gmail.com>
2013-10-25 16:20:55 -0500

    Deleted seed data because it was screwing up the db:reset.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Stock.create(
-    :company => "Google",
-    :stock_ticker => "GOOG",
-    :free_cash_flow => 16500,
-    :num_shares => 332, 
-    :PE_ratio => 25.8,
-    :beta => 1.19,
-    )
-Stock.create(
-    :company => "Microsoft",
-    :stock_ticker => "MSFT",
-    :free_cash_flow => 31626,
-    :num_shares => 8350,
-    :PE_ratio => 17.5,
-    :beta => 1.10,
-    )
-Stock.create( 
-    :company => "Yahoo",
-    :stock_ticker => "YHOO",
-    :free_cash_flow => -281,
-    :num_shares => 1008,
-    :PE_ratio => 6.67,
-    :beta => 0.89,
-    )
-Stock.create(
-    :company => "Facebook",
-    :stock_ticker => "FB",
-    :free_cash_flow => 1612,
-    :num_shares => 2420,
-    :PE_ratio => 872,
-    :beta => 1.33,
-    )
