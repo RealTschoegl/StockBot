fb0a6d70d8d6b8d3570f47e4b914ee22d698ea3c
RealTschoegl <Matt.Tschoegl@gmail.com>
2013-12-12 21:33:08 -0600

    I think this is how it's supposed to work.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,8 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://stockbot.io"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
-SitemapGenerator::Sitemap.sitemaps_host = "https://s3-us-west-2.amazonaws.com/#{ENV['FOG_DIRECTORY']}/sitemaps/sitemap.xml.gz"
-
+SitemapGenerator::Sitemap.sitemaps_host = "https://stockbot-sitemaps.s3-us-west-2.amazonaws.com/"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
