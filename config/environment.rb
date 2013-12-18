5d60da713f520c2c8cf09806f13499ff3ad3c8c7
RealTschoegl <Matt.Tschoegl@gmail.com>
2013-12-18 14:12:29 -0600

    Updated YUI manually and managing it through a global variable.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 StockPickApp::Application.initialize!
+
+$YUI_library_standard = "http://yui.yahooapis.com/3.13.0/build/yui/yui-min.js"
